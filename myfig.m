e237c8a185d43e60afb8a4f3b2bab4755411a40d
Martin Therkildsen <Martin.therkildsen@gmail.com>
2022-03-18 08:57:25 +0100

    Fix issue with myfig()

--- myfig.m
+++ myfig.m
@@ -8,14 +8,13 @@ function fig = myfig(fignumber, positionIn)
 
 	
 	defaultPosition = [0 500 700 500];	% Defualt position and width/height
-	sizePosIn = size(positionIn);		% Calculate size of input position
 
 	if nargin == 1
-		sprintf('nargin=1!')
 		% No position argument given: Use defualt position
 		position = defaultPosition;
 	elseif nargin == 2
-		sprintf('nargin=2!')
+		sizePosIn = size(positionIn);		% Calculate size of input position
+
 		% If only xlength and ylength are given, then use default position and
 		% change size only
 		if sizePosIn(2) == 2
