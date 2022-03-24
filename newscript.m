576c1fa03c8c4237109a6cdcfc9b7b3f2ecb7a88
Martin Therkildsen <Martin.therkildsen@gmail.com>
2022-03-24 14:53:10 +0100

    New function which makes template

---
+++ newscript.m
@@ -0,0 +1,24 @@
+function [] = newscript()
+% This script copes a script template to a selected folder with a selected
+% name and opens said script for editing. The selected script should be
+% named 'newScriptTemplate' and should be located in the same folder as
+% this function
+
+% Get function path (where the template file should be)
+[functionpath,~,~] = fileparts(mfilename('fullpath'))
+
+% Pop up save window to specify file name and path
+[file,path,indx] = uiputfile()
+
+% Concentate path and filename
+pathandname = strcat(path, file)
+
+%concentate function path and name of script
+funcpathnname = strcat(functionpath, '/newScriptTemplate.m')
+
+% Copy template from function folder to selected destination folder with
+% selected name
+copyfile(funcpathnname, pathandname)
+
+% Edit the file
+edit(pathandname)
