962a8ba0f2cf28146e2f6b52248d3d3f73a48501
Martin Therkildsen <Martin.therkildsen@gmail.com>
2022-03-24 16:21:12 +0100

    Changes to myfig to fix positions ending up outside screen

--- myfig.m
+++ myfig.m
@@ -6,24 +6,25 @@ function fig = myfig(fignumber, positionIn)
 	%			x and y are in the range 0 -> 1 where 0.5 is in the middle
 	% Outputs: figure handler
 	
-	% Default figure width and height
-	defwidth = 700; defheight = 500;
-	
 	% Extract screen resolution to define placement of figure
 	set(0,'units','pixels');	% Set unit for screen resolution extraction
 	screenres = get(0,'screensize'); screenres = screenres([3 4]);
 	screenXres = screenres(1); screenYres = screenres(2);
 
+
+	% Default figure width and height
+	defwidth = 700;
+	defheight = 500;
 	% Default figure x and y position (In middle of screen the the left)
-	defxpos = 0 * screenXres-defwidth/2;
-	defypos = 0.5 * screenYres-defheight/2;
+	defxpos = 0;
+	defypos = 0.5;
 	
 	% Defualt position and width/height
 	defaultPosition = [defxpos defypos defwidth defheight];
 
 	if nargin == 1
 		% No position argument given: Use defualt position
-		position = defaultPosition;
+		position = defaultPosition
 	elseif nargin == 2
 		sizePosIn = size(positionIn);		% Calculate size of input position
 
@@ -32,11 +33,30 @@ function fig = myfig(fignumber, positionIn)
 		if sizePosIn(2) == 2
 			position = [defaultPosition(1:2) positionIn];
 		else
-			xpos = positionIn(1) * screenXres-positionIn(3)/2;
-			ypos = positionIn(2) * screenYres-positionIn(4)/2;
-			position = [xpos ypos positionIn(3) positionIn(4)];
+			position = positionIn;
 		end
 	end
+
+	% Define x and y position based on screen resolution
+	xpos = position(1) * screenXres-position(3)/2
+	ypos = position(2) * screenYres-position(4)/2
+
+
+	% Contain the figure inside the screen if position is beyond borders
+	if xpos < 0
+		xpos = 0;
+	end
+	if ypos < 0
+		ypos = 0;
+	end
+	if xpos > (screenXres-position(3))
+		xpos = screenXres-position(3);
+	end
+	if ypos > (screenYres-position(4))
+		ypos = (screenYres-position(4));
+	end
+	
+	figPos = [xpos ypos position(3) position(4)];
 
 	if fignumber > 0
 		fig = figure(fignumber);
@@ -44,6 +64,6 @@ function fig = myfig(fignumber, positionIn)
 		fig = figure();
 	end
 	
-	fig.Position = position; fig.Color = 'white';
+	fig.Position = figPos; fig.Color = 'white';
 
 end
