289b4cbeeab4780151eab86ac33c710720302962
Martin Therkildsen <Martin.therkildsen@gmail.com>
2022-03-24 15:38:36 +0100

    Changes to script template and to myfig() function. x and y position is now defined between 0 and 1 0.5 = middle of screen

--- newScriptTemplate.m
+++ newScriptTemplate.m
@@ -2,10 +2,13 @@ clc;clear;close all;
 
 % Default figure size
 fwidth = 600; fheight = 400;
-% Default figure position
-fxpos = 0; fypos = 0;
-% x
-figpos = [fxpos fypos fwidth fheight]
+% Default position (numbers between 0 and 1)
+fXpos = 0; fYpos = 0.5;
+% Position and height/width
+figpos = [fXpos fYpos fwidth fheight];
+
+% Figure:
+% myfig(-1, figpos)
 
 
 % ========================================================================
