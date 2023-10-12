d02b2da030c36c524da1d4ab73153f616bcb604e
Martin Højlund Therkildsen <MROTR@vestas.com>
2023-10-12 16:14:07 +0200

    Modified myfigexport: Made code more clever and made the figure save folder act more cleverly

--- myfigexport.m
+++ myfigexport.m
@@ -1,4 +1,4 @@
-function myfigexport = myfigexport(saveDir, figures, fileNames, createNewFolder, folderName, resolution)
+function myfigexport = myfigexport(savePath, figures, fileNames, createSubFolder, subFoldeName, resolution)
 	% Export figures
 	% Inputs: saveDir, figures, fileNames, createNewFolder, folderName, resolution
 	% saveDir				: directory in '' string NOT "" string!!!!
@@ -9,37 +9,28 @@ function myfigexport = myfigexport(saveDir, figures, fileNames, createNewFolder,
 	% folderName			: folder name in '' string
 	% resolution			: absolute number.. Default: 400
 	
-	% Check if windows computer or mac
-	if ispc
-		% Windows
-		dirSplit = '\';
-	else
-		% Mac
-		dirSplit = '/';
-	end
-	
 	% Default resolution
 	if nargin == 5
 		resolution = 400;
 	end
-	
-	% If "\" or "/" at end of path. Remove it - otherwise an error occurs
-	if saveDir(end) == dirSplit
-		saveDir(end) == '';
-	end
 
 	% Just change savepath to whichever fits you!
-	if (createNewFolder == "true")
-		mkdir(saveDir, folderName);							% Create folder
-		savePath = strcat(saveDir, dirSplit, folderName);	% Save path for figures
+	if (createSubFolder == "true")
+		if isfolder(subFoldeName)
+			% do nothing if folder already exists
+			savePath = fullfile(savePath, subFoldeName);	% Save path for figures
+		else
+			mkdir(savePath, subFoldeName);							% Create folder
+			savePath = fullfile(savePath, subFoldeName);	% Save path for figures
+		end
 	else
-		savePath = saveDir;						% Save path for figures
+		savePath = savePath;		% Save path for figures
 	end
 	
 	fileName = fileNames;
 	
 	for i=1:length(figures)
-    	f = strcat(savePath, dirSplit, fileName(i));
+    	f = fullfile(savePath, fileName(i));
     	exportgraphics(figures(i), f,'Resolution', resolution);
 	end
 
